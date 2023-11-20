ea1accf45e0eeb2639e6fa8d2dbba6a1f586dff4
pranavdeshmukh28 <pranavdeshmukh28@gmail.com>
2023-11-20 16:50:45 +0530

    adding shell Script Files

---
+++ ShellScript/Script1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#This is my 1st shell script to print output
+echo "Hello All, Welcome to AWS/Devops Class"
+echo "Hello, How are you?"
+echo "Welcome to Young Minds"
+echo "Hello batch-21, We are learning Jenkins"
+echo "hello Team, How are you?"
+echo "Hiiii"

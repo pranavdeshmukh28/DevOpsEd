30b08acdc993127611a991632f118d32499faf87
pranavdeshmukh28 <pranavdeshmukh28@gmail.com>
2023-12-03 00:20:29 +0530

    cheery

--- ShellScript/Script1.sh
+++ ShellScript/Script1.sh
@@ -1,2 +1,14 @@
+<<<<<<< HEAD
+=======
+#!/bin/bash
+#This is my 1st shell script to print output
+echo "Hello All, Welcome to AWS/Devops Class"
+echo "Hello, How are you?"
+echo "Welcome to Young Minds"
+echo "Hello batch-21, We are learning Jenkins"
+echo "hello Team, How are you?"
+echo "Hasdfsdafsdfvsdfsd"
+echo "Hello this is some thing like cheerypick"
+>>>>>>> be255e2 (cheery)
 
 trytowork on reset
